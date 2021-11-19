85f1d5e08cf3b8fe4ce287210d9d7cf2e32cbcfb
amifidele <amifidele333@gmail.com>
2021-11-19 23:15:28 +0200

    add Calculator Model & It's table migration

---
+++ database/migrations/2021_11_19_210416_create_calculators_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCalculatorsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('calculators', function (Blueprint $table) {
+            $table->id();
+            $table->string('item_name');
+            $table->string('financing_type');
+            $table->integer('item_price');
+            $table->string('production_data');
+            $table->integer('self_deposit');
+            $table->integer('number_of_installment');
+            $table->string('most_caring');
+            $table->string('company_tax_id');
+            $table->string('phone_number');
+            $table->string('email');
+            $table->mediumText('additional_information');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('calculators');
+    }
+}
