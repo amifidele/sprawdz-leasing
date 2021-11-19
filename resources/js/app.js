8dc8eb16688bd4a02da37e5d4eb166d002c55b49
amifidele <amifidele333@gmail.com>
2021-11-19 15:39:26 +0200

    add inertia.js in laravel and reactjs

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,20 @@
-require('./bootstrap');
+import React from 'react';
+import { InertiaApp } from '@inertiajs/inertia-react';
+import { InertiaProgress } from '@inertiajs/progress';
+
+InertiaProgress.init({
+    color: '#ED8936',
+    showSpinner: true
+  });
+
+const app = document.getElementById('app');
+
+render(
+<InertiaApp
+    initialPage={JSON.parse(app.dataset.page)}
+    resolveComponent={name =>
+    import(`./Pages/${name}`).then(module => module.default)
+    }
+/>,
+app
+);
---
+++ resources/views/app.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head></head>
+
+{{-- @routes --}}
+<body class="antialiased">
+    @inertia
+</body>
+
+
+</html>
