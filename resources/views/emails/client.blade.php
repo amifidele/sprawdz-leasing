9f7ace162596800f082ed191b4fefd4fbb3e6bb5
amifidele <amifidele333@gmail.com>
2021-11-20 23:12:49 +0200

    add additional content in client email

--- resources/views/emails/client.blade.php
+++ resources/views/emails/client.blade.php
@@ -4,9 +4,57 @@
     </head>
     <body>
 
-        <h1>
-           Hellooooo
-        </h1>
+        <h1 style="text-size: 24px">
+          Item Name : {{$leasing->item_name}}
+        </h1><br>
+
+        <h1 style="text-size: 24px">
+          Financing Type : {{$leasing->item_name}}
+        </h1><br>
+
+        <h1 style="text-size: 24px">
+           Item Price : {{$leasing->financing_type}}
+        </h1><br>
+
+        <h1 style="text-size: 24px">
+          Production Data : {{$leasing->item_price}}
+        </h1><br>
+
+        <h1 style="text-size: 24px">
+           Self Deposit : {{$leasing->production_date}}
+        </h1><br>
+
+        <h1 style="text-size: 24px">
+           Self Deposit : {{$leasing->self_deposit}}
+        </h1><br>
+
+        <h1 style="text-size: 24px">
+          Number of installment : {{$leasing->number_of_installment}}
+        </h1><br>
+
+        <h1 style="text-size: 24px">
+          Redemption Value : {{$leasing->redemption_value}}
+        </h1><br>
+
+        <h1 style="text-size: 24px">
+           Most Caring : {{$leasing->most_caring}}
+        </h1><br>
+
+        <h1 style="text-size: 24px">
+           Company Tax Id :{{$leasing->company_tax_id}}
+        </h1><br>
+
+        <h1 style="text-size: 24px">
+           Phone Number : {{$leasing->phone_number}}
+        </h1><br>
+
+        <h1 style="text-size: 24px">
+          Email :  {{$leasing->email}}
+        </h1><br>
+
+        <h1 style="text-size: 24px">
+          Additional Information : {{$leasing->additional_information}}
+        </h1><br>
 
     </body>
 </html>
