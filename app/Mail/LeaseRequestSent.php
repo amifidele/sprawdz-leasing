c17fd181a7960be28e41067fcd6907daedf0b3ef
amifidele <amifidele333@gmail.com>
2021-11-20 12:30:35 +0200

    add leaseRequestSent Mail for client && setting mailtrap.io in .env

---
+++ app/Mail/LeaseRequestSent.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+
+use App\Models\Leasing;
+
+class LeaseRequestSent extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    public $leasing;
+
+    /**
+     * Create a new message instance.
+     *
+     * @return void
+     */
+    public function __construct(Leasing $leasing)
+    {
+        $this->leasing = $leasing;
+    }
+
+    /**
+     * Build the message.
+     *
+     * @return $this
+     */
+    public function build()
+    {
+        $subject = $this->leasing->objective;
+        return $this->to($this->leasing->email)
+                    ->subject("$subject Dziękujemy za skorzystanie z kalkulatora leasingu")
+                    ->view('view.name');
+    }
+}
