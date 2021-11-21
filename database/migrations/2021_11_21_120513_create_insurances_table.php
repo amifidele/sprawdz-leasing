2b33afd60144a326dd931a28be9ab80c079a8955
amifidele <amifidele333@gmail.com>
2021-11-21 14:49:57 +0200

    add NewCar controller, model & table

--- database/migrations/2021_11_21_120513_create_insurances_table.php
+++ database/migrations/2021_11_21_120513_create_insurances_table.php
@@ -15,6 +15,28 @@ class CreateInsurancesTable extends Migration
     {
         Schema::create('insurances', function (Blueprint $table) {
             $table->id();
+            $table->string('vehicle_type');
+            $table->string('brand');
+            $table->string('model');
+            $table->string('version')->nullable();
+            $table->string('engine_type');
+            $table->string('power');
+            $table->string('body_type');
+            $table->integer('vin_number');
+            $table->integer('vehicle_value');
+            $table->string('vehicle_financing');
+            $table->integer('year_of_production');
+            $table->string('first_registration_date');
+            $table->integer('vehicle_mileage');
+            $table->string('registration_postal_code');
+            $table->string('vehicle_parking_place');
+            $table->string('driving_license_issue_date');
+            $table->string('full_names');
+            $table->string('pesel_number')->nullable();
+            $table->integer('number_nip')->nullable();
+            $table->string('phone_number');
+            $table->string('email');
+            $table->string('company_address');
             $table->timestamps();
         });
     }
