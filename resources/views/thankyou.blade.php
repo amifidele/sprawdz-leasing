80d0199b37655bc6e230f056b35612c60e4bf2b5
amifidele <amifidele333@gmail.com>
2021-11-22 01:14:05 +0200

    add thankyou page

---
+++ resources/views/thankyou.blade.php
@@ -0,0 +1,18 @@
+@extends('layout')
+
+@section('content')
+
+    <div class="text-gray-600 text-center pt-16">
+        <h1 class="text-4xl font-bold">Thank you for submitting form</h1>
+
+        <img class="w-3/12 text-center m-auto mt-10" src="{{ asset('images/cropped-Logo@4x.png') }}">
+
+        <h1 class="text-3xl font-bold mt-6">
+            We will be back with replies as soon as possible.
+        </h1>
+
+       
+
+    </div>
+
+@endsection
