de3df971353a1603849281d160b0b3b9cae0f951
amifidele <amifidele333@gmail.com>
2021-11-21 17:36:57 +0200

    add field in insuranceController

--- app/Http/Controllers/InsuranceController.php
+++ app/Http/Controllers/InsuranceController.php
@@ -37,7 +37,29 @@ class InsuranceController extends Controller
     {
         
         $insurance = new Insurance;
-        $insurance->vehicle_type = request('vehicle')
+        $insurance->vehicle_type = request('vehicle');
+        $insurance->brand = request('brand');
+        $insurance->model = request('model');
+        $insurance->version = request('version');
+        $insurance->engine_type = request('engine_type');
+        $insurance->power = request('power');
+        $insurance->body_type = request('body_type');
+        $insurance->vin_number = request('vin_number');
+        $insurance->vehicle_value = request('vehicle_value');
+        $insurance->vehicle_financing = request('vehicle_financing');
+        $insurance->year_of_production = request('year_of_production');
+        $insurance->first_registration_date = request('first_registration_date');
+        $insurance->vehicle_mileage = request('vehicle_mileage');
+        $insurance->registration_postal_code = request('registration_postal_code');
+        $insurance->vehicle_parking_place = request('vehicle_parking_place');
+        $insurance->driving_license_issue_date = request('driving_license_issue_date');
+        $insurance->full_names = request('full_names');
+        $insurance->email = request('email');
+        $insurance->pesel_number = request('pesel_number');
+        $insurance->number_nip = request('number_nip');
+        $insurance->phone_number =  request('phone_number');
+        $insurance->company_address = request('company_address');
+        $insurance->save();
 
 
     }
