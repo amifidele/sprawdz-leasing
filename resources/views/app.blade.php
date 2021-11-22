b6880fa5abf5b10aef4f3d75ab5806103dce07b7
amifidele <amifidele333@gmail.com>
2021-11-22 02:26:38 +0200

    add cards on homepage

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -2,7 +2,7 @@
 
     @section('content')
     <div class="w-100">
-        <div class="absolute z-0 h-full" style="height: 450px">
+        <div class="absolute z-0 h-full" style="height: 470px">
             <img class="bg-container" 
             style="
             width: 100%;
@@ -12,14 +12,14 @@
             src="{{ asset('images/AdobeStock_252883460-scaled.jpeg') }}">
         </div>
 
-        <div class="flex w-100 relative z-1">
+        <div class="flex w-100 h-full relative z-1">
 
             <div class="flex-1 pl-12 pt-28 text-gray-800">
 
                 <div class="">
                     <h1 class="text-4xl pt-1 font-bold">The best</h1>
                     <h1 class="text-4xl pt-2 text-green-500 font-bold">Leasing Comparison</h1>
-                    <h1 class="text-4xl pt-2 font-bold">Available on the market</h1>
+                    <h1 class="text-4xl pt-2 font-bold">Available on the market.</h1>
                 </div>
 
                 <div class="mt-8">
@@ -39,10 +39,29 @@
 
         </div>
 
-        <div class="">
-
-        </div>
+        
         
     </div>
+
+
+    <div class="w-11/12 relative mx-auto z-9  mt-20 flex">
+
+        <div class="w-3/12 py-12 rounded bg-green-500">
+
+        </div>
+
+        <div class="w-3/12 ml-3 py-16 rounded bg-green-500">
+
+        </div>
+        <div class="w-3/12 ml-3 py-12 rounded bg-green-500">
+
+        </div>
+        <div class="w-3/12 ml-3 py-12 rounded bg-green-500">
+
+        </div>
+
+    </div>
+
+    <div class="h-36"></div>
 
     @endsection
